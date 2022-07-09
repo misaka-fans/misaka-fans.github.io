8fc224319e5da6767659fc3cc87c1be89e8f2aae
shbwb <104608787+misaka-fans@users.noreply.github.com>
2022-07-09 16:10:18 +0800

    Update secret.js

--- js/secret.js
+++ js/secret.js
@@ -1,3 +1,3 @@
 setTimeout(function(){
-	alert("6L+Z5Liq5b2p6JuL5rKh5pyJ5Lu75L2V55So \n 1100010 110110 110100")
+	alert("VGhpcyBpcyBhIHVzZWxlc3MgRWFzdGVyIGVnZw== \n 1100010 110110 110100")
 },15000);
