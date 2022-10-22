272aa655c6f16730b1114e07713a1b17c261faf3
shbwb <104608787+misaka-fans@users.noreply.github.com>
2022-10-22 13:58:47 +0800

    暂时性改图片地址
    暂时性更改，以便查看图片。等正常之后换回https://img.misaka-mikoto.jp/pic/，

--- scripts/img-link.js
+++ scripts/img-link.js
@@ -12,7 +12,7 @@ export default class ImgLink extends HTMLElement {
     return this.getAttribute('text');
   }
   get prefixUrl() {
-    return this.parentNode.getAttribute('prefix-url') || 'https://img.misaka-mikoto.jp/pic/';
+    return this.parentNode.getAttribute('prefix-url') || 'https://misaka-fans.netlify.app/pic/';
   }
   constructor() {
     super();
