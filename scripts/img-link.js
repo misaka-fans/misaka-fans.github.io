1b226682b4febbeb52d6712f2198c7de67bd1094
曾鼋棨 <yutou.1151.commu.reg@mail.ru>
2022-11-08 22:39:39 +0800

    Update img-link.js

--- scripts/img-link.js
+++ scripts/img-link.js
@@ -12,7 +12,7 @@ export default class ImgLink extends HTMLElement {
     return this.getAttribute('text');
   }
   get prefixUrl() {
-    return this.parentNode.getAttribute('prefix-url') || 'https://misaka-fans-photos.netlify.app/pic/';
+    return this.parentNode.getAttribute('prefix-url') || 'https://misaka-mikoto.jp/pic/';
   }
   constructor() {
     super();
